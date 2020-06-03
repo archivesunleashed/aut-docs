0b93b3c70780099fd4b1115614321ce52c12fafd
nruest <ruestn@gmail.com>
2020-06-02 20:04:41 -0400

    Test for ghpages publishing.

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,7 +21,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'archivesunleashed.github.io',
+  projectName: 'aut-docs',
   organizationName: 'archivesunleashed',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
